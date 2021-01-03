4217c5bcf7d83524d40abe1b135b2e2fe6c5c493
James <jqmesnz@gmail.com>
2021-01-03 19:05:31 +1300

    Update price emojis each time bot restarts

--- events/onReady.js
+++ events/onReady.js
@@ -8,5 +8,15 @@ module.exports = {
 
 		console.log(`Logged in as ${bot.user.tag}!`);
 		bot.user.setActivity('for new orders!', { type: 'WATCHING' });
+
+		const emojis = require('../config').prices;
+
+		bot.guilds.cache.each(guild => {
+			for (const emoji of Object.values(emojis)) {
+				if (!guild.emojis.cache.some(guildEmoji => guildEmoji.name === emoji.name)) {
+					guild.emojis.create(emoji.url, emoji.name);
+				}
+			}
+		});
 	},
 };
