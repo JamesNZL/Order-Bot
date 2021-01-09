a3c6dbc7c819ff264d92282eb3f0822bb06a7c37
James <jqmesnz@gmail.com>
2021-01-10 12:07:09 +1300

    Fix date mapping

--- modules/getTimestamp.js
+++ modules/getTimestamp.js
@@ -4,8 +4,12 @@ const moment = require('moment-timezone');
 
 module.exports = (lastWeekDay, startOfDay) => {
 	const currentDate = new Date(new Date().toLocaleString('en-US', { timeZone: 'America/Chicago' }));
-	const currentWeekMonday = currentDate.getDate() - currentDate.getDay() + 1;
-	const date = currentDate.setDate(currentWeekMonday - lastWeekDay);
+
+	const monday = (currentDate.getDay() >= 1)
+		? currentDate.getDate() - currentDate.getDay() + 1
+		: currentDate.getDate() - currentDate.getDay() + 8;
+
+	const date = currentDate.setDate(monday - lastWeekDay);
 	return (startOfDay)
 		? moment.tz(date, 'America/Chicago').startOf('day').valueOf()
 		: moment.tz(date, 'America/Chicago').endOf('day').valueOf();
