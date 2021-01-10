c42a2919a174bb4d36a83409c83089ffddf4290d
James <jqmesnz@gmail.com>
2021-01-10 14:05:11 +1300

    Check to ensure valid command channel

--- events/onMessageUpdate.js
+++ events/onMessageUpdate.js
@@ -6,7 +6,15 @@ module.exports = {
 	events: ['messageUpdate'],
 	process: [],
 	async execute(_, oldMessage, newMessage) {
-		if (newMessage.partial) return commandHandler(await newMessage.fetch());
-		if (oldMessage.content !== newMessage.content) return commandHandler(newMessage);
+		const msg = await newMessage.fetch();
+
+		const config = await require('../handlers/database')(msg.guild);
+
+		const validChannel = msg.channel.id === config.master.commands.id || !msg.guild;
+		const contentChanged = (oldMessage.partial)
+			? true
+			: oldMessage.content !== newMessage.content;
+
+		if (validChannel && contentChanged) return commandHandler(msg);
 	},
 };
